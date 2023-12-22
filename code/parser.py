569e88e264ca063e08bb808747981f0b2e309b58
Sebastian Rosales <sebastian.rosales.tejeda@gmail.com>
2023-12-22 14:36:41 -0300

    chore(infra): Add black replacing pep8.`

--- code/parser.py
+++ code/parser.py
@@ -1,6 +1,7 @@
 import json
 from typing import Dict, Optional, List, Tuple
 
+
 def json_parser(jsonInputString: str) -> Optional[Tuple]:
     json_input = json.loads(jsonInputString)
     player_one = json_input.get("player1")
@@ -15,7 +16,7 @@ def parse_movements(player: Dict) -> Optional[Dict]:
     movements = player.get("movimientos")
     if movements:
         return movements
-    
+
 
 def parse_hits(player: Dict) -> Optional[Dict]:
     hits = player.get("golpes")
