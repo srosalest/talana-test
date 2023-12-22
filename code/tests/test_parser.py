c801d6c9e91ecb4bbef8a0df08817cf11079e0f4
Sebastian Rosales <sebastian.rosales.tejeda@gmail.com>
2023-12-22 14:03:32 -0300

    chore(parser): Add test completition.

--- code/tests/test_parser.py
+++ code/tests/test_parser.py
@@ -5,22 +5,28 @@ from parser import json_parser, parse_movements, parse_hits
 
 class TestParser:
     @pytest.fixture
-    def jsonInputOne(self):
+    def json_valid_input(self):
         with open('code/json/input/example_1.json') as f:
             data = json.dumps(json.load(f))
             f.close()
             return data
+    
+    @pytest.fixture
+    def json_valid_input_empty_movement_and_hit_data(self):
+        with open('code/json/input/example_4.json') as f:
+            data = json.dumps(json.load(f))
+            f.close()
+            return data
 
-
-    def test_json_parser(self, jsonInputOne):
+    def test_given_valid_input_when_json_parser_then_return_parsed_data(self, json_valid_input):
         # Given 
-        jsonInputString = jsonInputOne
+        json_input_string = json_valid_input
 
         # When
-        parsed_data = json_parser(jsonInputString)
+        parsed_data = json_parser(json_input_string)
 
         # Then
-        assert json_parser(jsonInputOne) == (
+        assert parsed_data == (
             [
                 ['D', 'DSD', 'S', 'DSD', 'SD'], 
                 ['K', 'P', '', 'K', 'P']
@@ -30,11 +36,30 @@ class TestParser:
                  ['K', '', 'K', 'P', 'P'],
             ],
         )
-   
+    
+    def test_given_valid_input_when_json_parser_then_return_parsed_data(self, json_valid_input_empty_movement_and_hit_data):
+        # Given 
+        json_input_string = json_valid_input_empty_movement_and_hit_data
 
-    def test_parse_movements(self, jsonInputOne):
+        # When
+        parsed_data = json_parser(json_input_string)
+
+        # Then
+        assert parsed_data == (
+            [
+                None, 
+                ['P', '']
+            ],
+            [
+                ['', 'ASA', 'DA', 'AAA', '', 'SA'],
+                None,
+            ],
+        )
+     
+           
+    def test_given_player_movement_data_when_parse_movements_then_return_player_movement(self, json_valid_input):
         # Given
-        json_input = json.loads(jsonInputOne)
+        json_input = json.loads(json_valid_input)
         player_one = json_input.get("player1")
         player_two = json_input.get("player2")
 
@@ -46,9 +71,23 @@ class TestParser:
         assert  movements_player_one == ['D', 'DSD', 'S', 'DSD', 'SD']
         assert  movements_player_two == ['SA', 'SA', 'SA', 'ASA', 'SA']
 
-    def test_parse_hits(self, jsonInputOne):
+    def test_given_player_empty_movement_data_when_parse_movements_then_return_empty_list(self, json_valid_input_empty_movement_and_hit_data):
         # Given
-        json_input = json.loads(jsonInputOne)
+        json_input = json.loads(json_valid_input_empty_movement_and_hit_data)
+        player_one = json_input.get("player1")
+        player_two = json_input.get("player2")
+
+        # When
+        movements_player_one = parse_movements(player_one)
+        movements_player_two = parse_movements(player_two)
+
+        # Then
+        assert  movements_player_one == None
+        assert  movements_player_two == ['', 'ASA', 'DA', 'AAA', '', 'SA']
+
+    def test_given_player_valid_date_when_parse_hits_return_player_hits(self, json_valid_input):
+        # Given
+        json_input = json.loads(json_valid_input)
         player_one = json_input.get("player1")
         player_two = json_input.get("player2")
 
@@ -59,3 +98,17 @@ class TestParser:
         # Then
         assert  hits_player_one == ['K', 'P', '', 'K', 'P']
         assert  hits_player_two == ['K', '', 'K', 'P', 'P']
+
+    def test_given_player_empty_hit_data_when_parse_hits_then_return_empty_list(self, json_valid_input_empty_movement_and_hit_data):
+            # Given
+            json_input = json.loads(json_valid_input_empty_movement_and_hit_data)
+            player_one = json_input.get("player1")
+            player_two = json_input.get("player2")
+
+            # When
+            movements_player_one = parse_hits(player_one)
+            movements_player_two = parse_hits(player_two)
+
+            # Then
+            assert  movements_player_one == ['P', '']
+            assert  movements_player_two == None
